93078d3ba064e4eea36c6d2abda35443ae772037
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2017-11-24 17:42:22 +0000

    about to start favouriting

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get '/u/:id', to: 'users#show', as: 'user'     
   end
   resources :messages, except: :destroy
+  
   get '/success', to: 'messages#success'
   
   delete '/messages/:id', to: 'messages#destroy_sent', as: 'delete_sent_message'
